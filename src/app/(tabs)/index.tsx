cb70b52c8dcf2fd67bd777d6aa20e3e8c91d2ac9
bojanciko <122114035+bojanciko@users.noreply.github.com>
2024-02-26 18:32:21 +0100

    added product details page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,31 +1,10 @@
-import { StyleSheet } from 'react-native';
+import React from 'react'
+import { Redirect } from 'expo-router'
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+const TabIndex = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab 1</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+    <Redirect href={'/menu/'} />
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default TabIndex
---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,78 @@
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
+import React, { useState } from 'react'
+import { Stack, useLocalSearchParams } from 'expo-router'
+import products from '@assets/data/products'
+import { defaultPizzaImage } from '@/components/ProductListItem'
+import Colors from '@/constants/Colors'
+import Button from '@/components/Button'
+
+
+const sizes = ['S', 'M', 'L', 'XL']
+
+const ProductDetailsScreen = () => {
+  const [selectedSize, setSelectedSize] = useState<string>('M')
+  const { id } = useLocalSearchParams()
+
+  const product = products.find(product => product.id.toString() === id)
+
+  const addToCart = () => {
+    console.warn('Add to cart', product?.name, selectedSize)
+  }
+
+  if (!product) {
+    return <Text>Product not found!</Text>
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name, headerTitleAlign: 'center' }} />
+      <Image source={{ uri: product?.image || defaultPizzaImage }} style={{ width: '100%', aspectRatio: 1 }} />
+      <Text>Select size</Text>
+      <Text></Text>
+      <View style={styles.sizes}>
+        {sizes.map(size => (
+          <Pressable onPress={() => setSelectedSize(size)} style={[styles.size, { backgroundColor: selectedSize === size ? 'gainsboro' : 'white' }]} key={size}>
+            <Text style={[styles.text, { color: selectedSize === size ? 'black' : 'gray' }]}>{size}</Text>
+          </Pressable>
+        ))}
+      </View>
+      <Text style={{ fontSize: 20 }}>${product.price}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
+    </View>
+  )
+}
+
+
+
+export default ProductDetailsScreen
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  sizes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: 'gainsboro',
+    borderRadius: 25,
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+  },
+  text: {
+
+  }
+})
